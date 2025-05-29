8d57b1a6e72d4d08c9c86f7c9ee861e2be16ced4
X <x5368x@gmail.com>
2025-05-29 09:40:50 -0500

    Actualizar getVol.sh

--- .config/eww/scripts/getVol.sh
+++ .config/eww/scripts/getVol.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-if command -v pamixer >/dev/null 2>&1; then
+# Si existe wpctl (PipeWire)
+if command -v wpctl >/dev/null 2>&1; then
+    if wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -q MUTED; then
+        echo 0
+    else
+        wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf("%d\n", $3 * 100)}'
+    fi
+
+# Si no, usa pamixer como fallback
+elif command -v pamixer >/dev/null 2>&1; then
     if [ "$(pamixer --get-mute)" = "true" ]; then
         echo 0
-        exit
     else
         pamixer --get-volume
     fi
+
+# Última opción: amixer (muy legado)
 else
     amixer -D pulse sget Master | awk -F '[^0-9]+' '/Left:/{print $3}'
 fi
